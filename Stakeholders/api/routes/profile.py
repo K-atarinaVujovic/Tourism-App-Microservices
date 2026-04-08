c67f632a0e6882bcf3e926953c611d214a4d5641
Katarina Vujovic <v.katar551@gmail.com>
2026-04-09 01:28:49 +0200

    feat: Implement api layer

---
+++ Stakeholders/api/routes/profile.py
@@ -0,0 +1,42 @@
+from typing import Annotated, Any
+
+from fastapi import Body, FastAPI, HTTPException, Path
+
+from core.exceptions import AlreadyExistsException, NotFoundException
+from schemas.profile import ProfileCreate, ProfileResponse, ProfileUpdate
+from services.profile import ProfileService
+
+app = FastAPI()
+service = ProfileService()
+
+@app.get("/profiles/{profile_id}", response_model=ProfileResponse)
+async def get_profile(
+  profile_id: Annotated[int, Path()]
+  ) -> Any:
+  try:
+    response = service.get(profile_id)
+    return response
+  except NotFoundException as e:
+    raise HTTPException(status_code=404, detail=str(e))
+
+@app.post("/profiles/create", response_model=ProfileResponse)
+async def create_profile(
+  profile: Annotated[ProfileCreate, Body()]
+) -> Any:
+  try:
+    response = service.create(profile)
+    return response
+  except AlreadyExistsException as e:
+    raise HTTPException(status_code=409, detail=str(e))
+
+@app.put("/profiles/update", response_model=ProfileResponse)
+async def update_profile(
+  profile: Annotated[ProfileUpdate, Body()],
+) -> Any:
+  try:
+    # Get logged in user to set profile id...
+    profile_id = 1
+    return service.update(profile_id, profile)
+  except NotFoundException as e:
+    raise HTTPException(status_code=404, detail=str(e))
+
