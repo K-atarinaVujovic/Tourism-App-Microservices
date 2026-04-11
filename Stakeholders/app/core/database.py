9c405a07958255ff8248c8f8aa4c74792fd4f4c4
Katarina Vujovic <v.katar551@gmail.com>
2026-04-12 00:34:03 +0200

    refactor: Use env value for database url

--- Stakeholders/app/core/database.py
+++ Stakeholders/app/core/database.py
@@ -1,10 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+import os
 
 class Base(DeclarativeBase):
     id: Mapped[int] = mapped_column(primary_key=True)
 
-engine = create_engine("postgresql+psycopg2://postgres:root@localhost:5432/stakeholders")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://postgres:root@localhost:5432/stakeholders")
+
+engine = create_engine(DATABASE_URL)
 
 def init_db():
     Base.metadata.create_all(engine, checkfirst=True)
