a12a9b0931487f96d491c5524ebf9fbc063bc873
Katarina Vujovic <v.katar551@gmail.com>
2026-05-28 13:32:39 +0200

    feat: Update grpc pb files, run both API and gRPC simultaneously

--- Stakeholders/main.py
+++ Stakeholders/main.py
@@ -17,29 +17,47 @@ from app.schemas.profile import ProfileCreate, ProfileUpdate
 from app.services.profile import ProfileService
 from app.services.upload import Uploader
 
+import threading
+import signal
+import sys
+import asyncio
+
+grpc_server = None
 
 GRPC_PORT = int(os.getenv("STAKEHOLDERS_GRPC_PORT", 50051))
 FASTAPI_PORT = int(os.getenv("STAKEHOLDERS_FASTAPI_PORT", 8000))
 LISTEN_ADDR = "[::]:" + str(GRPC_PORT)
 
-def serve_fastapi():
+async def serve_fastapi():
     print(f"Swagger UI: http://127.0.0.1:{FASTAPI_PORT}/docs")
-    uvicorn.run("app.api.routes.profile:app", host="0.0.0.0", port=FASTAPI_PORT, reload=True)
+    # uvicorn.run("app.api.routes.profile:app", host="0.0.0.0", port=FASTAPI_PORT, reload=True)
+    config = uvicorn.Config("app.api.routes.profile:app", host="0.0.0.0", port=FASTAPI_PORT, reload=True)
+    uvicorn_server = uvicorn.Server(config)
+    await uvicorn_server.serve()
 
-def serve_grpc():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+async def serve_grpc():
+    global grpc_server
+    grpc_server = grpc.aio.server(futures.ThreadPoolExecutor(max_workers=10))
     profile_service = ProfileService()
-    upload_service = Uploader()
-    profile_pb2_grpc.add_ProfileServiceServicer_to_server(ProfileServicer(profile_service, upload_service), server)
-    server.add_insecure_port(LISTEN_ADDR)
-    server.start()
-    print("Server started, listening on " + str(GRPC_PORT))
-    server.wait_for_termination()
-    return server
+    profile_pb2_grpc.add_ProfileServiceServicer_to_server(ProfileServicer(profile_service), grpc_server)
+    grpc_server.add_insecure_port(LISTEN_ADDR)
+    await grpc_server.start()
+    print("gRPC server started, listening on " + str(GRPC_PORT))
+    await grpc_server.wait_for_termination()
+    return grpc_server
+
+async def shutdown(sig, frame):
+    print("Shutting down...")
+    if grpc_server:
+        await grpc_server.stop(grace=0)
+    sys.exit(0)
+
+async def main():
+    grpc_task = asyncio.create_task(serve_grpc())
+    await serve_fastapi()
+    await grpc_server.stop(grace=5) #type: ignore
 
 if __name__ == "__main__":
-    # logging.basicConfig()
-    serve_fastapi()
-    # serve_grpc()
+    asyncio.run(main())
 
 
