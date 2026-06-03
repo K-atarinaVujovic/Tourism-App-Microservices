6aead00f28a0c496c71df023d72edf9b8ea74325
Emi7i <emilijaopsenica@gmail.com>
2026-06-03 17:38:26 +0200

    Refine auth for refund method and add tour prices

--- Stakeholders/app/api/routes/profile.py
+++ Stakeholders/app/api/routes/profile.py
@@ -80,7 +80,7 @@ async def update_balance_for_user(
         current_user = Depends(get_current_user),
 ) -> Any:
   try:
-    if current_user["role"] != "admin":
+    if current_user[user_id] != 1:
       raise HTTPException(status_code=403, detail="Unauthorized")
     response = await service.update_balance(user_id, balance)
     return response
