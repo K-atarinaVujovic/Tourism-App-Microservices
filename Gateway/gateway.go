4b971364a7822ee1e0e85f2beb12d7279189e19e
Kristina Petrovic <petrovickristina718@gmail.com>
2026-06-03 20:01:33 +0200

    feat: implement tracing

--- Gateway/gateway.go
+++ Gateway/gateway.go
@@ -18,6 +18,16 @@ import (
 	servicepb "gateway/proto/service"
 	tourspb "gateway/proto/tours"
 	"jwtreader"
+
+    "time"
+
+    "go.opentelemetry.io/otel"
+    "go.opentelemetry.io/otel/attribute"
+    "go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+    "go.opentelemetry.io/otel/sdk/resource"
+    sdktrace "go.opentelemetry.io/otel/sdk/trace"
+    semconv "go.opentelemetry.io/otel/semconv/v1.24.0"
+    "go.opentelemetry.io/otel/trace"
 )
 
 // MicroserviceRegistry Should contain all generated API handlers
@@ -257,7 +267,69 @@ func CORSMiddleware(next http.Handler) http.Handler {
     })
 }
 
+var tracer trace.Tracer
+
+// Initializing Tracer
+func InitTracer(ctx context.Context) (*sdktrace.TracerProvider, error) {
+	endpoint := os.Getenv("OTEL_EXPORTER_OTLP_ENDPOINT")
+	if endpoint == "" {
+		endpoint = "jaeger:4318"
+	}
+
+	exporter, err := otlptracehttp.New(
+		ctx,
+		otlptracehttp.WithEndpoint(endpoint),
+		otlptracehttp.WithInsecure(),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithBatcher(exporter),
+		sdktrace.WithResource(resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceName("api-gateway"),
+		)),
+	)
+
+	otel.SetTracerProvider(tp)
+	tracer = otel.Tracer("api-gateway")
+
+	return tp, nil
+}
+
+func TracingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx, span := tracer.Start(r.Context(), r.Method+" "+r.URL.Path)
+		defer span.End()
+
+		start := time.Now()
+
+		span.SetAttributes(
+			semconv.HTTPMethod(r.Method),
+			semconv.HTTPRoute(r.URL.Path),
+			attribute.String("http.target", r.URL.RequestURI()),
+		)
+
+		next.ServeHTTP(w, r.WithContext(ctx))   //withContext -> ako budemo propagirali tracing ka mikroservisima
+
+		span.SetAttributes(
+			attribute.String("duration", time.Since(start).String()),
+		)
+	})
+}
+
 func main() {
+    ctx := context.Background()
+    tp, err := InitTracer(ctx)
+    if err != nil {
+        log.Fatal("failed to initialize tracer: ", err)
+    }
+    defer func() {
+        _ = tp.Shutdown(ctx)
+    }()
+
 	// Load config
 	config, err := LoadConfig("CONFIG.yaml")
 	if err != nil {
@@ -276,8 +348,11 @@ func main() {
 	// Wrap with JWT authentication middleware
 	authHandler := JWTAuthMiddleware(config.ExcludedPaths, proxyHandler)
 
+	// Handle tracing
+    tracingHandler := TracingMiddleware(authHandler)
+
 	// Handle cors
-    corsHandler := CORSMiddleware(authHandler)
+    corsHandler := CORSMiddleware(tracingHandler)
 
 	// Handle all requests to the server using the authenticated proxy
 	http.Handle("/", corsHandler)
