002c8cacc7777b4bc0329f636a383282552dece0
Katarina Vujovic <v.katar551@gmail.com>
2026-06-04 00:25:50 +0200

    feat: Add some logs to stakeholders

--- Stakeholders/app/services/profile.py
+++ Stakeholders/app/services/profile.py
@@ -3,6 +3,8 @@ from app.repositories.profile import ProfileRepository
 from app.schemas.profile import ProfileCreate, ProfileResponse, ProfileUpdate
 from app.core.exceptions import NotFoundException, AlreadyExistsException
 
+import logging
+logger = logging.getLogger(__name__)
 class ProfileService:
   repo: ProfileRepository = ProfileRepository()
 
@@ -21,6 +23,7 @@ class ProfileService:
     saved = await self.repo.save(new_profile)
     data = saved.model_dump() # because uhhh getting id from doc db or something idk
     data["id"] = str(saved.id)
+    logger.info(f"Profile created for {new_profile.user_id}")
     return ProfileResponse.model_validate(data)
 
   async def update(self, user_id: int, updated_profile_schema: ProfileUpdate) -> ProfileResponse:
@@ -37,6 +40,7 @@ class ProfileService:
     updated = await self.repo.update(profile)
     data = updated.model_dump()
     data["id"] = str(updated.id)
+    logger.info(f"Profile updated for {user_id}")
     return ProfileResponse.model_validate(data)
 
   async def get(self, profile_id: str) -> ProfileResponse:
@@ -53,4 +57,5 @@ class ProfileService:
       raise NotFoundException("Profile not found")
     data = profile.model_dump()
     data["id"] = str(profile.id)
+    logger.info(f"Profile fetched for {user_id}")
     return ProfileResponse.model_validate(data)
